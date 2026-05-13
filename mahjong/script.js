63e647e60ef208178b9f5906f85fc9204e488665
Yoryu <yoryushi@gmail.com>
2026-05-14 02:10:11 +0900

    4人点数管理機能追加

--- mahjong/script.js
+++ mahjong/script.js
@@ -1,13 +1,32 @@
-let score = 25000;
+const players = [
+  { name: "A", score: 25000 },
+  { name: "B", score: 25000 },
+  { name: "C", score: 25000 },
+  { name: "D", score: 25000 }
+];
 
-function addScore() {
-  score += 1000;
+function addScore(index) {
+  players[index].score += 1000;
 
-  document.getElementById("scoreA").textContent = score;
+  updateScreen();
 }
 
-function minusScore() {
-  score -= 1000;
+function minusScore(index) {
+  players[index].score -= 1000;
 
-  document.getElementById("scoreA").textContent = score;
+  updateScreen();
+}
+
+function updateScreen() {
+  document.getElementById("score0").textContent =
+    players[0].score;
+
+  document.getElementById("score1").textContent =
+    players[1].score;
+
+  document.getElementById("score2").textContent =
+    players[2].score;
+
+  document.getElementById("score3").textContent =
+    players[3].score;
 }
